0bd5705f8b7835f8df13dd55a746f410a7444bfd
robertnjunge <mubearobert@gmail.com>
2023-07-11 08:25:54 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,46 @@
+function add() {
+    add (a + b);
+}
+function subtract() {
+    subtract (a - b);
+}
+function multiply() {
+    multiply (a * b);
+}
+function divide() {
+    divide (a / b);
+}
+
+function add(a, b) {
+return (a +b);
+}
+
+ function subtract(a, b) {
+    return (a -b);
+ }
+
+ function multiply(a, b) {
+    return(a * b);
+ }
+
+function divide(a, b) {
+    return(a / b);
+}
+
+function increment(n) {
+    n++;
+    return n;
+}
+
+function decrement(n) {
+    n--;
+    return n;
+}
+
+function makeInt(n) {
+    return parseFloat(n);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
